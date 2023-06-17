e8fcd48392e10737e29bd824215acef8c3c3a916
Jeremy Oliver <joliver427@icloud.com>
2023-06-17 17:04:20 -0600

    commit tree 87cf9a4b75859310dbc2cdc830299de131f7cd01

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+
+  const [cart, setCart] = useState("")
+
+  function handleClick() {
+    if (cart === "") {
+      setCart((cart) => "in-cart")
+    } else {
+      setCart((cart) => "")
+    }
+  }
+
   return (
-    <li className="">
+    <li className={cart}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleClick}>Add to Cart</button>
     </li>
   );
 }
